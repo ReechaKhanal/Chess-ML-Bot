3c5fdbc6530a7f4fde40c7a760134b1663626555
rkhanal <rkhanal@uno.edu>
2021-02-10 17:27:52 -0600

    created a mechanism for a Normalized file creation from raw data, Needs more data cleaning

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from normalizeData import normalizeData
 
 def main():
 	nd = normalizeData()
-	data_dict = nd.readFile()
-	
+	(nd.getNormalizedFile()).to_csv("NormalizedFile.csv")
+
+
 if __name__ == '__main__':
 	main()
--- normalizeData.py
+++ normalizeData.py
@@ -1,3 +1,6 @@
+from collections import defaultdict;
+import pandas as pd
+
 class normalizeData():
 
 	def init(self):
@@ -6,9 +9,47 @@ class normalizeData():
 	def getNormalizedFile(self):
 
 		# convert the data in form of a dictionary
-		data_dict = self.readFile()
+		all_columns, data_dict = self.readFile()
 
-		# get 'excel' rows from dictionary
+		# change data_dict to contain all_columns:
+		data_dict = self.modifyDataDict(all_columns, data_dict)
+
+		# get a finalized and easy to use dictionary
+		data_dict = self.getFinalizedDataDict(all_columns, data_dict)
+
+		# convert dictionary to dataframe
+		df = pd.DataFrame.from_dict(data_dict)
+
+		return df
+
+
+	def getFinalizedDataDict(self, all_columns, data_dict):
+
+		final_dict = defaultdict(list)
+
+		for index, inner_dict in data_dict.items():
+
+			for key, value in inner_dict.items():
+
+				final_dict[key].append(value)
+
+		return final_dict
+
+
+	def modifyDataDict(self, all_columns, data_dict):
+
+		for index, inner_dict in data_dict.items():
+
+			for column_name in all_columns:
+				if column_name not in inner_dict:
+
+					inner_dict[column_name] = None
+
+			data_dict[index] = inner_dict
+
+		return data_dict
+
+
 
 	def readFile(self):
 
@@ -16,6 +57,8 @@ class normalizeData():
 		inputFile = open('ChessDataReecha.pgn', 'r')
 		lines = inputFile.readlines()
 		file_length = len(lines)
+		all_columns = set()
+		all_columns.add("moves")
 
 		index = 0
 		column_data, total_data = {}, {}
@@ -28,7 +71,7 @@ class normalizeData():
 			if line == "":
 				pass
 
-			elif line.endswith("0-1") or line.endswith("1-0"):
+			elif line.endswith("0-1") or line.endswith("1-0") or line.endswith("1/2-1/2"):
 				column_data["moves"] = line
 				total_data[index] = column_data
 				index = index+1
@@ -40,6 +83,8 @@ class normalizeData():
 
 				index_of_seperation = line.index(' ')
 				key, value = line[:index_of_seperation], line[index_of_seperation:]
-
+				all_columns.add(key)
 				column_data[key] = value
-		return total_data
+		
+		
+		return all_columns, total_data
